d6d812928133a6cdfa7d1c4a71ef0c21f6aa0c2a
Marzieh Vahabi <taranom15@gmail.com>
2018-10-16 22:50:47 +0200

    Add files via upload

---
+++ webapp/vsearch4web_old.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template, request, escape
+from vsearch import search4letters
+
+app = Flask(__name__)
+
+def log_request(req:'flask_request', res:str)-> None:
+	with open('vsearch.log','a') as log:
+		print(req.form, req.remote_addr, req.user_agent, res, file=log, sep='|')
+
+@app.route('/search4',methods=['POST'])
+def do_search() -> 'html':
+	phrase = request.form['phrase']
+	letters = request.form['letters']
+	title = 'Here are your result:'
+	results = str(search4letters(phrase, letters))
+	return render_template('results.html', the_phrase=phrase, the_letters=letters, the_title=title, the_results=results,)
+
+@app.route('/')
+@app.route('/entry')
+def entry_page() -> 'html':
+	return render_template('entry.html',the_title='Welcome to search4letters on the web!')
+
+@app.route('/viewlog')
+def view_the_log() -> 'html':
+	contents = []
+	with open('vsearch.log') as log:
+		for line in log:
+			contents.append([])
+			for item in line.split('|'):
+				contents[-1].append(escape(item))
+	title = ('From Data', 'Remote_addr', 'User_agent', 'Result')
+	return render_template('viewlog.html', the_title='View Log', the_row_titles=titles, the_data=contents,)
+
+if __name__ == '__main__':
+	app.run(debug=True)
