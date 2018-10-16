84d188df405a4a5a0b7109856f30a422cc7891f1
Marzieh Vahabi <taranom15@gmail.com>
2018-10-16 21:56:29 +0200

    Add files via upload

---
+++ mymodules/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(
+    name='vsearch',
+    version='1.0',
+    description='The Head First Python Search Tools',
+    author='MV',
+    author_email='taranom15@gmail.com',
+    url='',
+    py_modules=['vsearch'],
+)
